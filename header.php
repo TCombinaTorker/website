9d4aa24e658594206ab263266ee89d240eac6eaa
Yagiz Gunes Teker <gunes.teker@gmail.com>
2022-04-29 15:32:08 +0200

    fixed

--- header.php
+++ header.php
@@ -5,28 +5,28 @@
     <!-- The navigation menu -->
     <div class="navbar">
         <div class="subnav">
-            <button onclick="window.location.href='//localhost:3000/index.php?p=home'" class="subnavbtn">Home<i></i></button>
+            <button onclick="window.location.href='./index.php?p=home'" class="subnavbtn">Home<i></i></button>
         </div>
         <div class="subnav">
-            <button onclick="window.location.href='//localhost:3000/index.php?p=subjects'" class="subnavbtn">Subjects <i
+            <button onclick="window.location.href='./index.php?p=subjects'" class="subnavbtn">Subjects <i
                     ></i></button>
             <div class="subnav-content">
-                <button onclick="window.location.href='//localhost:3000/index.php?p=func&n=1'">Functional Programming</button>
-                <button onclick="window.location.href='//localhost:3000/index.php?p=cla&n=1'">Linguistics Applications</button>
-                <button onclick="window.location.href='//localhost:3000/index.php?p=ir&n=1'">Information Retrieval Models</button>
-                <button onclick="window.location.href='//localhost:3000/index.php?p=crypt&n=1'">Cryptography</button>
-                <button onclick="window.location.href='//localhost:3000/index.php?p=math&n=1'">Mathematics</button>
+                <button onclick="window.location.href='./index.php?p=func&n=1'">Functional Programming</button>
+                <button onclick="window.location.href='./index.php?p=cla&n=1'">Linguistics Applications</button>
+                <button onclick="window.location.href='./index.php?p=ir&n=1'">Information Retrieval Models</button>
+                <button onclick="window.location.href='./index.php?p=crypt&n=1'">Cryptography</button>
+                <button onclick="window.location.href='./index.php?p=math&n=1'">Mathematics</button>
                 
             </div>
         </div>
         <div class="subnav">
-            <button onclick="window.location.href='//localhost:3000/index.php?p=about'" class="subnavbtn">About<i></i></button>
+            <button onclick="window.location.href='./index.php?p=about'" class="subnavbtn">About<i></i></button>
         </div>
         <div class="subnav">
-            <button onclick="window.location.href='//localhost:3000/index.php?p=cv'" class="subnavbtn">CV<i></i></button>
+            <button onclick="window.location.href='./index.php?p=cv'" class="subnavbtn">CV<i></i></button>
         </div>
         <div class="subnav">
-            <button onclick="window.location.href='//localhost:3000/index.php?p=contact'" class="subnavbtn">Contact<i></i></button>
+            <button onclick="window.location.href='./index.php?p=contact'" class="subnavbtn">Contact<i></i></button>
         </div>
     </div>
 
